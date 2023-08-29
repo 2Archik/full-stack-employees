770d9e1f51ba695ae0162ad3c5c168d2d8820586
2Archik <artur.avitolog@yandex.ru>
2023-08-29 09:45:31 +0500

    Написал мидлвару для записи токена в LS, написал сервисы и слайс для сотрудников

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,14 +1,19 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import auth from "../features/auth/authSlice";
+import employees from "../features/employees/employeesSlice";
 import { api } from "./services/api";
+import { listenerMiddleware } from "../middlewares/auth";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth,
+    employees,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+    getDefaultMiddleware()
+      .concat(api.middleware)
+      .prepend(listenerMiddleware.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,6 +9,7 @@ import { Paths } from "./paths";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import "./index.css";
+import { Auth } from "./features/auth/auth";
 
 const router = createBrowserRouter([
   {
@@ -36,7 +37,9 @@ root.render(
           algorithm: theme.darkAlgorithm,
         }}
       >
-        <RouterProvider router={router} />
+        <Auth>
+          <RouterProvider router={router} />
+        </Auth>
       </ConfigProvider>
     </Provider>
   </React.StrictMode>
