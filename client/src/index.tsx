fdbd5579c0c625c2be0fbb93799985c8d54aa30f
2Archik <artur.avitolog@yandex.ru>
2023-08-30 11:11:01 +0500

    fix and deploy

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -14,6 +14,7 @@ import { Employees } from "./pages/employees";
 import { AddEmployee } from "./pages/add-employee";
 import { Status } from "./pages/status";
 import { Employee } from "./pages/employee";
+import { EditEmployee } from "./pages/edit-employee";
 
 const router = createBrowserRouter([
   {
@@ -39,6 +40,10 @@ const router = createBrowserRouter([
   {
     path: `${Paths.employee}/:id`,
     element: <Employee />,
+  },
+  {
+    path: `${Paths.employeeEdit}/:id`,
+    element: <EditEmployee />,
   },
 ]);
 
--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -3,15 +3,41 @@ import { Card, Form, Row, Space, Typography } from "antd";
 import { CustomInput } from "../../components/custom-input";
 import { PasswordInput } from "../../components/password-input";
 import { CustomButton } from "../../components/custom-button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Paths } from "../../paths";
+import { useState } from "react";
+import { useRegisterMutation } from "../../app/services/auth";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { User } from "@prisma/client";
+import { ErrorMessage } from "../../components/error-message";
+
+type RegisterData = Omit<User, "id"> & { confirmPassword: string };
 
 export const Register = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [registerUser] = useRegisterMutation();
+
+  const register = async (data: RegisterData) => {
+    try {
+      await registerUser(data).unwrap();
+      navigate("/");
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err);
+
+      if (maybeError) {
+        setError(err.data.message);
+      } else {
+        setError("Неизвестная ошибка");
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Зарегистрируйтесь" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={register}>
             <CustomInput name="name" placeholder="Имя" />
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Пароль" />
@@ -27,6 +53,7 @@ export const Register = () => {
             <Typography.Text>
               Уже зарегистрированы? <Link to={Paths.login}>Войдите</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
